2ba30274b282ee4d48fc6c7a0a4d49f0f84c1e27
Gary Bailey <garybailey@mac.com>
2020-08-17 22:19:02 +0100

    first commit for trasp.sh

--- trasp.sh
+++ trasp.sh
@@ -1,4 +1,4 @@
-cd /home/pi/tsensors
+cd /home/pi/tsensor
 TVAL=$(cat /sys/class/thermal/thermal_zone0/temp)
 let "TVAL /=1000"
-curl -d "rasp4_temp value=$TVAL" http://rasp4.local:8086/write?db=rasp_monitor
+curl -d "rasp7_temp value=$TVAL" http://rasp4.local:8086/write?db=rasp_monitor
