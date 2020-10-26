517e6175b7653499da27380dfc96af7206533283
Gary Bailey <garybailey@mac.com>
2020-10-26 08:10:52 +0000

    Added tread2.sh to repo

--- tread2.sh
+++ tread2.sh
@@ -1,3 +1,3 @@
-cd /home/pi/tsensors
+cd /home/pi/tsensor
 TVAL=$(python3 tread4.py)
-curl -d "room_temp value=$TVAL" http://rasp4.local:8086/write?db=room_monitor
+curl -d "outside_temp value=$TVAL" http://rasp4.local:8086/write?db=room_monitor
